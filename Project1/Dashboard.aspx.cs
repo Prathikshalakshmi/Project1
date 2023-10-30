afec511a2e7ec8a4f3ba21bf994e28bc575fc30d
Akhi70328 <120740514+Akhi70328@users.noreply.github.com>
2023-10-30 11:36:47 +0530

    Update Dashboard.aspx.cs

--- Project1/Dashboard.aspx.cs
+++ Project1/Dashboard.aspx.cs
@@ -6,18 +6,15 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
 namespace Project1
 {
     public partial class Dashboard : System.Web.UI.Page
     {
         private readonly object lblMessage;
-
         protected void btnLogout_Click(object sender, EventArgs e)
         {
             // Sign the user out
             System.Web.Security.FormsAuthentication.SignOut();
-
             // Redirect to the login page or any other desired page
             Response.Redirect("Login.aspx");
         }
@@ -25,13 +22,11 @@ namespace Project1
         {
             // Get the selected option from the dropdown.
             string selectedValue = ddlTemplates.SelectedValue;
-
             if (!string.IsNullOrEmpty(selectedValue))
             {
                 // Redirect to the selected page.
                 Response.Redirect(selectedValue);
             }
-
         }
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -40,7 +35,6 @@ namespace Project1
                 // You can perform any initialization here.
             }
         }
-
         protected void btnUpload_Click(object sender, EventArgs e)
         {
             if (fileUpload.HasFile)
@@ -49,18 +43,15 @@ namespace Project1
                 {
                     // Get the uploaded file's content.
                     Stream stream = fileUpload.PostedFile.InputStream;
-
                     using (StreamReader reader = new StreamReader(stream))
                     {
                         // Create a DataTable to hold the CSV data.
                         DataTable dt = new DataTable();
                         bool firstRow = true;
-
                         while (!reader.EndOfStream)
                         {
                             string line = reader.ReadLine();
                             string[] values = line.Split(',');
-
                             if (firstRow)
                             {
                                 // Create columns based on the first row of the CSV.
@@ -81,11 +72,9 @@ namespace Project1
                                 dt.Rows.Add(row);
                             }
                         }
-
                         // Bind the DataTable to the GridView.
                         GridView1.DataSource = dt;
                         GridView1.DataBind();
-                        
                     }
                 }
                 catch (Exception ex)
@@ -93,7 +82,6 @@ namespace Project1
                     // Handle any exceptions (e.g., invalid CSV format).
                     Response.Write("Error: " + ex.Message);
                 }
-
             }
         }
     }
